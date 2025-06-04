813bcf30225710ad28adceebc3f5317fe09d1e02
Joao Pereira <nashi.codes@gmail.com>
2025-06-03 21:48:16 -0300

    initialize project with Maven, Spring Boot, and necessary configurations

---
+++ src/main/java/com/jp/eletrohub/EletrohubApplication.java
@@ -0,0 +1,13 @@
+package com.jp.eletrohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletrohubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EletrohubApplication.class, args);
+    }
+
+}
