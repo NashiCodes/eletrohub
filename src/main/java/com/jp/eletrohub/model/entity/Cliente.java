718848a4143096ef9cd397f8d5efd64aca546c1e
Pedro <pedro.lourenco@estudante.ufjf.br>
2025-06-05 22:20:04 -0300

    Adiciona argumentos à classe cliente

--- src/main/java/com/jp/eletrohub/model/entity/Cliente.java
+++ src/main/java/com/jp/eletrohub/model/entity/Cliente.java
@@ -1,9 +1,17 @@
 package com.jp.eletrohub.model.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
 
@@ -12,7 +20,7 @@ public class Cliente {
     private Long id;
 
     private String nome;
-    private String CPF;
+    private String cpf;
     private String telefone;
     private String email;
 }
