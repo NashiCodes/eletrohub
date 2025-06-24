2bcc9d58178cb8facc981df55b4bfb9e006eebb8
Joao Pereira <nashi.codes@gmail.com>
2025-06-24 01:02:52 -0300

    Adiciona arquivos de configuração para a infraestrutura do EletroHub

---
+++ tools/infra.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+SUFFIX=${2:-dev}
+NAME="eletrohub-${SUFFIX}"
+
+start=false
+stop=false
+resetdb=false
+status=false
+
+usage()
+{
+    echo "Usage: ./tools/infra.sh [OPTION]"
+    echo "Runs EletroHub's basic infrastructure in development mode"
+    echo "Examples:"
+    echo "          infra start [NAME]"
+    echo "          infra stop [NAME]"
+    echo "          infra resetdb [NAME]"
+    echo "          infra status [NAME]"
+    echo ""
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        start )                                       start=true
+                                                      ;;
+        stop )                                        stop=true
+                                                      ;;
+        resetdb )                                     resetdb=true
+                                                      ;;
+        status )                                      status=true
+                                                      ;;
+        -h | --help )                                 usage
+                                                      exit 2
+                                                      ;;
+        * )
+    esac
+    shift
+done
+
+cd $(readlink -f $(dirname $0))
+
+if [ "$status" = true ]; then
+    docker container ls --filter "name=eletrohub-${SUFFIX}"
+    exit 0
+fi
+
+if [ "$start" = true ]; then
+    docker compose --project-name $NAME up -d --build
+    exit 0
+fi
+
+if [ "$stop" = true ]; then
+    docker compose --project-name $NAME down || :
+    exit 0
+fi
+
+
+if [ "$resetdb" = true ]; then
+    docker compose --project-name "$NAME" rm -f -s -v postgres
+    docker volume rm "${NAME}_pgdata"
+    docker compose --project-name "$NAME" up -d postgres
+    exit 0
+fi
+
+usage
